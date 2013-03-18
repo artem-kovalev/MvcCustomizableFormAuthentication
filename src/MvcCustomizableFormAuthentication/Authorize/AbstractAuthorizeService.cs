dcab85882851074c1801cbcb72293178e0780e61
nomit007 <nomit007@gmail.com>
2013-03-18 23:57:24 +0700

    Add Authorize Service

---
+++ src/MvcCustomizableFormAuthentication/Authorize/AbstractAuthorizeService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MvcCustomizableFormAuthentication
+{
+	public class AbstractAuthorizeService
+	{
+		public AbstractAuthorizeService ()
+		{
+		}
+	}
+}
+
---
+++ src/MvcCustomizableFormAuthentication/Authorize/IAuthorizeService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MvcCustomizableFormAuthentication
+{
+	public interface IAuthorizeService
+	{
+	}
+}
+
